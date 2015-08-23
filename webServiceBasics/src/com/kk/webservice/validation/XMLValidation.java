154a8aaaaab277eed9c9e66558ac06c3f0586ef7
krishna <krishna880@gmail.com>
2015-08-23 18:00:49 -0500

    Initial commit for web service basics project - kk

---
+++ webServiceBasics/src/com/kk/webservice/validation/XMLValidation.java
@@ -0,0 +1,12 @@
+package com.kk.webservice.validation;
+
+public class XMLValidation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.println("kk is learning!!!");
+			System.out.println("kk is learning java!!!");
+	}
+
+}
